33904b659952a276b8acb01783d26d3754f40166
wchouyu <thacinfun@gmail.com>
2017-10-25 16:25:25 +0800

    test

--- list.h
+++ list.h
@@ -73,7 +73,7 @@ public:
 		   //vector<Term*>v{err};
 		   //List return_list_buf(v);
 		   //List *return_list=new List(return_list_buf);
-		   throw std::out_of_range("Accessing tail in an empty list");
+		   throw "Accessing tail in an empty list";
 		   //return return_list;
 	  }
   }
